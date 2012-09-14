d0a961dd4f421d80ce1434588dad0cec6a38b1c9
nrkkalyan <nrkkalyan@gmail.com>
2012-09-14 22:22:08 +0200

    MyCountries

--- Colors/src/com/lnu/ColorDisplay.java
+++ Colors/src/com/lnu/ColorDisplay.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.Spanned;
-import android.text.method.DigitsKeyListener;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
---
+++ MyCountries/src/com/lnu/MyCountries.java
@@ -0,0 +1,52 @@
+package com.lnu;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+public class MyCountries extends ListActivity {
+	
+	static final Set<String>	countryVisitedList	= new HashSet<String>();
+	{
+		countryVisitedList.add("2005 France");
+		countryVisitedList.add("2006 UK");
+		countryVisitedList.add("2007 USA");
+		
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_countries);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		adapter.addAll(countryVisitedList);
+		adapter.sort(String.CASE_INSENSITIVE_ORDER);
+		setListAdapter(adapter);
+		
+		Button addNewCountryButton = (Button) findViewById(R.id.addNewCountryButton);
+		addNewCountryButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(MyCountries.this, AddCountry.class);
+				MyCountries.this.startActivity(myIntent);
+			}
+		});
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_my_countries, menu);
+		return true;
+	}
+}
